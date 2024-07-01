cbf625a75609114973a854484b77ecab3df4bb97
kaoutarOuadih1 <kaoutar.ouadih8@gmail.com>
2024-07-01 21:28:37 +0100

    contact form using angular and taking accessibility in consideration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'contact-form';
+  contact: Contact = new Contact();
+  isFormSubmited: boolean = false;
+  isSubmitBtnClicked: boolean = false;
+
+  onSubmit(contactForm){
+    this.isSubmitBtnClicked = true;
+    if(contactForm.form.valid){
+      this.isFormSubmited = true;
+      this.isSubmitBtnClicked = false;
+      console.log(contactForm.value);
+      contactForm.reset();
+      setTimeout(()=>{this.isFormSubmited = false}, 3000);
+    }
+  }
 }
---
+++ src/app/contact.ts
@@ -0,0 +1,8 @@
+export class Contact {
+    firstName: string;
+    lastName: string;
+    email: string;
+    queryType: string;
+    message: string;
+    contactPreference: boolean;
+}
